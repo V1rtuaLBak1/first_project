e7b37845a2100ccf606bc93be741f757f572984b
V1rtuaL <SupremeComanderFA@mail.ru>
2023-08-19 18:03:22 +0300

    second practice

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
+'use strict';
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
-const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const ratingOfFilms = prompt("На сколько оцените его?", "");
 const personalMovieDB = {
     count : numberOfFilms ,
     movies : {},
@@ -9,4 +8,36 @@ const personalMovieDB = {
     privat : false
 };
 personalMovieDB.movies[lastFilm] = ratingOfFilms;
+
+for (let i = 0 ; i < 2; i++)
+{
+    const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+          ratingOfFilms = prompt("На сколько оцените его?", "");
+    
+    if (lastFilm != null && ratingOfFilms != null && lastFilm != '' && ratingOfFilms != '' && lastFilm.length < 50 )
+    {
+        personalMovieDB.movies[lastFilm] = ratingOfFilms;
+        console.log("done");
+    }
+    else {
+        console.log(error);
+        i--;
+    }
+
+}
+
+if (personalMovieDB < 10)
+{
+    console.log('Просмотрено довольно мало фильмов');
+}
+else if (personalMovieDB >= 10 && personalMovieDB <= 30)
+{
+    console.log("Вы классический зритель");
+}
+else if (personalMovieDB >= 30)
+{
+    console.log('Вы киноман');
+}
+else {console.log("Произошла ошибка")}
 console.log(personalMovieDB);
+
